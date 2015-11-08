0156858390c5d9020d2144d5877ec19963457e24
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-08 02:29:27 -0500

    copy tests

--- test/copy.js
+++ test/copy.js
@@ -3,29 +3,55 @@
 require('mocha');
 var fs = require('fs');
 var path = require('path');
+var argv = require('minimist')(process.argv.slice(2), {
+  alias: {debug: 'd'}
+});
 var assert = require('assert');
+var once = require('../lib/once');
 var copy = require('..');
 var del = require('delete');
 
 function exists(fp, cb) {
+  if (argv.debug) return cb();
+
   fs.exists(fp, function(exists) {
     if (!exists) {
       cb(new Error('expected file to exist'));
+    } else {
+      del(path.dirname(fp), cb);
     }
-    del(path.dirname(fp), cb);
   });
 }
 
 describe('copy', function() {
-  it('should copy a file', function(cb) {
+  it.skip('should copy a file', function(cb) {
     var src = 'test/fixtures/a.txt';
     var dest = 'test/actual/a.txt';
 
     copy(src, dest, function(err) {
       if (err) return cb(err);
-      console.log(arguments)
-      // exists(dest, cb);
-      cb()
+      exists(dest, cb);
+    });
+  });
+
+  it('should copy a glob of files', function(cb) {
+    var src = 'test/fixtures/*.txt';
+    var dest = 'test/actual';
+
+    copy(src, dest, function(err) {
+      if (err) return cb(err);
+      exists(dest, cb);
+    });
+  });
+
+  it.only('should copy a glob of files from a cwd', function(cb) {
+    var src = '*.txt';
+    var dest = 'test/actual';
+    var opts = {cwd: 'test/fixtures/'};
+
+    copy(src, dest, opts, function(err) {
+      if (err) return cb(err);
+      exists(dest, cb);
     });
   });
 
@@ -42,7 +68,7 @@ describe('copy', function() {
 
   it('should throw an error when callback is not a function', function(cb) {
     try {
-      copy('foo/');
+      copy('foo/', 'bar');
       cb(new Error('expected an error'));
     } catch (err) {
       assert(err);
