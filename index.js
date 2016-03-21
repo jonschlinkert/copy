f1a98b29c7a3309ad0b1fd987ae6e543320ff38a
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-21 07:03:01 -0400

    expose `copy.each` and `copy.one`, add docs

--- index.js
+++ index.js
@@ -8,14 +8,32 @@ var recurse = require('./lib/recurse');
 var utils = require('./lib/utils');
 var base = require('./lib/base');
 
+/**
+ * Copy a filepath, vinyl file, array of files, or glob of files to the
+ * given destination `directory`, with `options` and callback function that
+ * exposes `err` and the array of vinyl files that are created by the copy
+ * operation.
+ *
+ * ```js
+ * copy('*.js', 'dist', function(err, file) {
+ *   // exposes the vinyl `file` created when the file is copied
+ * });
+ * ```
+ * @param {String|Object|Array} `patterns` Filepath(s), vinyl file(s) or glob of files.
+ * @param {String} `dir` Destination directory
+ * @param {Object|Function} `options` or callback function
+ * @param {Function} `cb` Callback function if no options are specified
+ * @api public
+ */
+
 function copy(patterns, dir, options, cb) {
+  if (arguments.length < 3) {
+    return invalid.apply(null, arguments);
+  }
+
   if (typeof options === 'function') {
     cb = options;
     options = {};
-  }
-
-  if (arguments.length < 3) {
-    return invalid.apply(null, arguments);
   }
 
   var opts = utils.extend({cwd: process.cwd()}, options);
@@ -23,14 +41,13 @@ function copy(patterns, dir, options, cb) {
   patterns = utils.arrayify(patterns);
 
   if (!utils.hasGlob(patterns)) {
-    opts.isGlob = false;
     copyEach(patterns, dir, opts, cb);
     return;
   }
 
+  opts.patterns = patterns;
   if (!opts.srcBase) {
     opts.srcBase = path.resolve(opts.cwd, utils.parent(patterns));
-    opts.base = path.relative(opts.cwd, opts.srcBase);
   }
 
   utils.glob(patterns, opts, function(err, files) {
@@ -39,19 +56,71 @@ function copy(patterns, dir, options, cb) {
   });
 }
 
+/**
+ * Copy an array of files to the given destination `directory`, with
+ * `options` and callback function that exposes `err` and the array of
+ * vinyl files that are created by the copy operation.
+ *
+ * ```js
+ * copy.each(['foo.txt', 'bar.txt', 'baz.txt'], 'dist', function(err, files) {
+ *   // exposes the vinyl `files` created when the files are copied
+ * });
+ * ```
+ * @name .copy.each
+ * @param {Array} `files` Filepaths or vinyl files.
+ * @param {String} `dir` Destination directory
+ * @param {Object|Function} `options` or callback function
+ * @param {Function} `cb` Callback function if no options are specified
+ * @api public
+ */
+
 function copyEach(files, dir, options, cb) {
+  if (arguments.length < 3) {
+    return invalid.apply(null, arguments);
+  }
+
+  if (typeof options === 'function') {
+    cb = options;
+    options = {};
+  }
+
+  var opts = utils.extend({}, options);
+  if (typeof opts.cwd === 'undefined') {
+    opts.cwd = process.cwd();
+  }
+
+  if (!opts.srcBase && opts.patterns) {
+    opts.srcBase = path.resolve(opts.cwd, utils.parent(patterns));
+  }
+
   async.reduce(files, [], function(acc, filepath, next) {
-    if (options.isGlob === false) {
-      filepath = path.resolve(options.cwd, filepath);
-    }
-    copyOne(filepath, dir, options, function(err, file) {
+    filepath = path.resolve(opts.cwd, filepath);
+    copyOne(filepath, dir, opts, function(err, file) {
       if (err) return next(err);
       next(null, acc.concat(file));
     });
   }, cb);
 }
 
-function copyOne(fp, dir, options, cb) {
+/**
+ * Copy a single `file` to the given `dest` directory, using
+ * the specified options and callback function.
+ *
+ * ```js
+ * copy.one('foo.txt', 'dist', function(err, file) {
+ *   if (err) throw err;
+ *   // exposes the vinyl `file` that is created when the file is copied
+ * });
+ * ```
+ * @name .copy.one
+ * @param {String|Object} `file` Filepath or vinyl file
+ * @param {String} `dir` Destination directory
+ * @param {Object|Function} `options` or callback function
+ * @param {Function} `cb` Callback function if no options are specified
+ * @api public
+ */
+
+function copyOne(file, dir, options, cb) {
   if (typeof options === 'function') {
     cb = options;
     options = {};
@@ -61,53 +130,12 @@ function copyOne(fp, dir, options, cb) {
     return invalid.apply(null, arguments);
   }
 
-  toDest(dir, fp, options, function(err, file) {
-    base(fp, file.path, options, function(err) {
+  toDest(dir, file, options, function(err, out) {
+    base(file, out.path, options, function(err) {
       if (err) return cb(err);
-      cb(null, file);
+      cb(null, out);
     });
   });
-}
-
-function mapDest(file, dest, options) {
-  var opts = utils.extend({}, options);
-  var base = opts.srcBase || utils.parent(opts.patterns);
-
-  // if (utils.hasGlob(patterns)) {
-  //   opts.isGlob = true;
-  //   opts.mapDest = true;
-  // }
-  // if (Array.isArray(patterns)) {
-  //   opts.mapDest = true;
-  // }
-  // if (dest.slice(-1) === '/') {
-  //   opts.mapDest = true;
-  // }
-}
-
-/**
- * Normalize options and ensure that destination directory
- * and file paths will result in what the user expects.
- *
- * @param {String|Array} `patterns` Glob pattern(s) or file path(s)
- * @param {String} `dest` Desination directory for copied files
- * @param {Object} `options`
- * @return {Object}
- */
-
-function createOptions(patterns, dest, options) {
-  var opts = utils.extend({overwrite: true}, options);
-  if (utils.hasGlob(patterns)) {
-    opts.isGlob = true;
-    opts.mapDest = true;
-  }
-  if (Array.isArray(patterns)) {
-    opts.mapDest = true;
-  }
-  if (dest.slice(-1) === '/') {
-    opts.mapDest = true;
-  }
-  return opts;
 }
 
 /**
@@ -116,3 +144,4 @@ function createOptions(patterns, dest, options) {
 
 module.exports = copy;
 module.exports.one = copyOne;
+module.exports.each = copyEach;
