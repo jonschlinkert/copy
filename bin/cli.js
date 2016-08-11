0f2df6952ba6c83b8c2966a18c80797f011ef869
jonschlinkert <jon.schlinkert@sellside.com>
2016-08-11 08:41:38 -0400

    support `--cwd`

--- bin/cli.js
+++ bin/cli.js
@@ -2,19 +2,22 @@
 
 var copy = require('..');
 var log = require('log-ok');
-var argv = process.argv.slice(2);
-var dir = argv.pop();
-var patterns = argv;
+var argv = require('yargs-parser')(process.argv.slice(2), {
+  alias: {src: 's', dest: 'd', cwd: 'c'},
+  default: {cwd: process.cwd()},
+});
 
-if (!patterns || !dir) {
-  console.log('Usage: copy <patterns> <dir>');
+var src = argv.src || argv._[0];
+var dest = argv.dest || (argv.src ? argv._[0] : argv._[1]);
+
+if (!src || !dest) {
+  console.log('Usage: copy <src_patterns> <dest_directory>');
 } else {
-  copy(patterns, dir, function(err, files) {
+  copy(src, dest, argv, function(err, files) {
     if (err) {
       console.error(err);
       process.exit(1);
     }
-
     files.forEach(function(file) {
       log(file.relative);
     });
